0dec8f1228446ec9359bbd4973b859a800e85eb5
= <=>
2017-10-21 17:24:40 -0700

    made the table beautiful

---
+++ table.js
@@ -0,0 +1,40 @@
+function getMaxLength(data, fieldName) {
+  const allLengthsOfField = data.map(d => String(d[fieldName]).length);
+  return Math.max(...allLengthsOfField);
+}
+
+function pad(value, maxlength) {
+  if (value.length < maxlength) {
+    return value + getChars(" ", maxlength - value.length);
+  }
+  return value;
+}
+
+function getChars(char, length) {
+  var characters = "";
+  for (var i = 0; i < length; i++) {
+    characters += char;
+  }
+  return characters;
+}
+
+const delim = "\t\t";
+const fieldNames = ["id", "product_name", "department_name", "price", "stock_quantity"];
+function printTable(data) {
+  const lengths = fieldNames.map(fieldName => getMaxLength(data, fieldName));
+  const headers = fieldNames.map((fieldName, index) => pad(fieldName, lengths[index])).join(delim);
+
+  console.log(headers + "\n");
+
+  for (var i = 0; i < data.length; i++) {
+    console.log(
+      fieldNames
+      .map((fieldName, index) => {
+        return pad(data[i][fieldName], lengths[index]);
+      })
+      .join(delim)
+    );
+  }
+}
+
+module.exports = printTable;
--- zamazoncustomer.js
+++ zamazoncustomer.js
@@ -1,4 +1,27 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var printTable = require("./table");
 
-var connection 
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user:"root",
+    password: "rina0728",
+    database: "zamazon_DB"
+});
+
+connection.connect(function(error) {
+    if(error) throw error;
+    // callbackfunction
+    readProducts();
+});
+
+function readProducts() {
+    console.log("Here are the most updated list of products that we have");
+    connection.query("SELECT * FROM zamazon", function(err, res) {
+        // if (err) throw err;
+        // console.log(res);
+        printTable(res);
+    });
+}
+
